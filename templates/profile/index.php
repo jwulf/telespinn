b06799400c051a8bbce614fa7b1d7fc0f51b27e9
Lasse Brenden <lasse.brenden@icloud.com>
2013-11-17 15:07:59 +0100

    Refactor table styles

--- templates/profile/index.php
+++ templates/profile/index.php
@@ -8,76 +8,93 @@
             <div class="grid-column grid-span-12">
               <h2>Inspirasjon</h2>
               <p>
-                Har du laget noe flott med våre produkter? Vis oss; inspirer andre strikkere; og/eller få tilbakemeldinger og tips!
+                Har du laget noe flott med våre produkter? Eller har du kanskje noen gode tips? Del det du kan med andre strikkere! 
               </p>
               <p>
-                Nedenfor finner du alle dine innsendte artikler:
+                Nedenfor finner du alle dine innsendte inspirasjonsartikler:
               </p>
-              <table class="table">
-                <tr class="table-row">
-                  <th class="table-header" width="30%">
-                    Tittel
-                  </th>
-                  <th class="table-header" width="10%">
-                    Innsendt
-                  </th>
-                  <th class="table-header" width="10%">
-                    Publisert
-                  </th>
-                  <th class="table-header" width="50%"></th>
-                </tr>
-                <tr class="table-row">
-                  <td class="table-cell">
-                    <a href="/">Artikkel 3</a>
-                  </td>
-                  <td class="table-cell">
-                    01.12.2013
-                  </td>
-                  <td class="table-cell">
-                    01.12.2013
-                  </td>
-                  <td class="table-cell table-cell-options">
-                    <a class="button-light button-red">Fjern artikkel</a>
-                  </td>
-                </tr>
-                <tr class="table-row">
-                  <td class="table-cell">
-                    <a href="/">Artikkel 2</a>
-                  </td>
-                  <td class="table-cell">
-                    30.11.2013
-                  </td>
-                  <td class="table-cell">
-                    30.11.2013
-                  </td>
-                  <td class="table-cell table-cell-options">
-                    <a class="button-light button-red">Fjern artikkel</a>
-                  </td>
-                </tr>
-                <tr class="table-row">
-                  <td class="table-cell">
-                    <a href="/">Artikkel 1</a>
-                  </td>
-                  <td class="table-cell">
-                    01.11.2013
-                  </td>
-                  <td class="table-cell">
-                    01.11.2013
-                  </td>
-                  <td class="table-cell table-cell-options">
-                    <a class="button-light button-red">Fjern artikkel</a>
-                  </td>
-                </tr>
-              </table>
+              <div class="table">
+                <div class="table-wrapper">
+                  <div class="table-header">
+                    <div class="table-cell table-cell-span-3">
+                      Tittel
+                    </div>
+                    <div class="table-cell table-cell-span-1">
+                      Type
+                    </div>
+                    <div class="table-cell table-cell-span-1">
+                      Innsendt
+                    </div>
+                    <div class="table-cell table-cell-span-1">
+                      Publisert
+                    </div>
+                    <div class="table-cell table-cell-span-4"></div>
+                  </div>
+                  <div class="table-body">
+                    <div class="table-row">
+                      <div class="table-cell table-cell-span-3">
+                        <a href="/">Artikkel 3</a>
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        Generell
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        01.11.2013
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        02.11.2013
+                      </div>
+                      <div class="table-cell table-cell-span-4 table-options">
+                        <a class="button-light button-red">Fjern artikkel</a>
+                      </div>
+                    </div>
+                    <div class="table-row">
+                      <div class="table-cell table-cell-span-3">
+                        <a href="/">Artikkel 2</a>
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        Produkt
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        01.10.2013
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        02.10.2013
+                      </div>
+                      <div class="table-cell table-cell-span-4 table-options">
+                        <a class="button-light button-red">Fjern artikkel</a>
+                      </div>
+                    </div>
+                    <div class="table-row">
+                      <div class="table-cell table-cell-span-3">
+                        <a href="/">Artikkel 1</a>
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        Generell
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        01.09.2013
+                      </div>
+                      <div class="table-cell table-cell-span-1">
+                        02.09.2013
+                      </div>
+                      <div class="table-cell table-cell-span-4 table-options">
+                        <a class="button-light button-red">Fjern artikkel</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div class="grid-row">
             <div class="grid-column grid-span-6">
-              <a class="button-dark button-green">Skriv ny artikkel</a>
+              <a class="button-dark button-green">Skriv artikkel</a>
+              <a class="button-light button-green">Skriv produktartikkel</a>
             </div>
             <div class="grid-align-right grid-column grid-span-6">
               <div class="float-right">
-                <a class="button-light button-red">Fjern alle mine artikler</a>
+                <a class="button-dark button-red">Fjern alle mine artikler</a>
               </div>
             </div>
           </div>
@@ -89,65 +106,81 @@
           <p>
             Nedenfor finner du alle dine tidligere ordre:
           </p>
-          <table class="table">
-            <tr class="table-row">
-              <th class="table-header" width="20%">
-                Ordrenummer
-              </th>
-              <th class="table-header" width="10%">
-                Bestilt
-              </th>
-              <th class="table-header" width="20%">
-                Beløp
-              </th>
-              <th class="table-header" width="50%"></th>
-            </tr>
-            <tr class="table-row">
-              <td class="table-cell">
-                3
-              </td>
-              <td class="table-cell">
-                15.11.2013
-              </td>
-              <td class="table-cell">
-                NOK 750,00
-              </td>
-              <td class="table-cell table-cell-options">
-                <a class="button-light button-green">Se faktura</a>
-                <a class="button-light button-green">Bestill ordre på nytt</a>
-              </td>
-            </tr>
-            <tr class="table-row">
-              <td class="table-cell">
-                2
-              </td>
-              <td class="table-cell">
-                01.11.2013
-              </td>
-              <td class="table-cell">
-                NOK 1 000,00
-              </td>
-              <td class="table-cell table-cell-options">
-                <a class="button-light button-green">Se faktura</a>
-                <a class="button-light button-green">Bestill ordre på nytt</a>
-              </td>
-            </tr>
-            <tr class="table-row">
-              <td class="table-cell">
-                1
-              </td>
-              <td class="table-cell">
-                15.10.2013
-              </td>
-              <td class="table-cell">
-                NOK 500,00
-              </td>
-              <td class="table-cell table-cell-options">
-                <a class="button-light button-green">Se faktura</a>
-                <a class="button-light button-green">Bestill ordre på nytt</a>
-              </td>
-            </tr>
-          </table>
+          <div class="table">
+            <div class="table-wrapper">
+              <div class="table-header">
+                <div class="table-cell table-cell-span-3">
+                  Ordre
+                </div>
+                <div class="table-cell table-cell-span-1">
+                  Bestilt
+                </div>
+                <div class="table-cell table-cell-span-1">
+                  Levert
+                </div>
+                <div class="table-cell table-cell-span-1">
+                  Beløp
+                </div>
+                <div class="table-cell table-cell-span-4"></div>
+              </div>
+              <div class="table-body">
+                <div class="table-row">
+                  <div class="table-cell table-cell-span-3">
+                    <a href="/">125</a>
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    01.11.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    02.11.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    1 500,00
+                  </div>
+                  <div class="table-cell table-cell-span-4 table-options">
+                    <a class="button-light button-green">Se faktura</a>
+                    <a class="button-light button-green">Bestill ordre på nytt</a>
+                  </div>
+                </div>
+                <div class="table-row">
+                  <div class="table-cell table-cell-span-3">
+                    <a href="/">124</a>
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    01.10.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    02.10.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    1 000,00
+                  </div>
+                  <div class="table-cell table-cell-span-4 table-options">
+                    <a class="button-light button-green">Se faktura</a>
+                    <a class="button-light button-green">Bestill ordre på nytt</a>
+                  </div>
+                </div>
+                <div class="table-row">
+                  <div class="table-cell table-cell-span-3">
+                    <a href="/">123</a>
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    01.09.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    02.09.2013
+                  </div>
+                  <div class="table-cell table-cell-span-1">
+                    500,00
+                  </div>
+                  <div class="table-cell table-cell-span-4 table-options">
+                    <a class="button-light button-green">Se faktura</a>
+                    <a class="button-light button-green">Bestill ordre på nytt</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
