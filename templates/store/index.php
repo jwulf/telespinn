ece1f565249567bb3ba5881991806c0c4a627c69
Lasse Brenden <lasse.brenden@icloud.com>
2013-11-19 16:06:56 +0100

    Add store page view

--- templates/store/index.php
+++ templates/store/index.php
@@ -2,6 +2,158 @@
   <div class="container">
     <div id="page">
       <h1>Butikk</h1>
+      <div class="grid-row">
+        <div class="grid-column grid-span-12">
+          <h2>Garn</h2>
+          <div class="grid-row">
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-garn-1.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Pan 283</h3>
+                      <p class="description">
+                        Pan 283 er et slitesterkt<br>3-tråds strikkegarn.
+                      </p>
+                      <p class="price">
+                        kr 115
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 90%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-garn-2.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Pan 001</h3>
+                      <p class="description">
+                        Pan 001 er et slitesterkt<br>3-tråds strikkegarn.
+                      </p>
+                      <p class="price">
+                        kr 115
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 25%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-garn-3.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Pan 365</h3>
+                      <p class="description">
+                        Pan 365 er et slitesterkt<br>3-tråds strikkegarn.
+                      </p>
+                      <p class="price">
+                        kr 115
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 90%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid-row">
+        <div class="grid-column grid-span-12">
+          <h2>Tekstiler</h2>
+          <div class="grid-row">
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-tekstiler-1.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Kidmohairsjal</h3>
+                      <p class="description">
+                        Vevet sjal av Marigarn med renning i silke.
+                      </p>
+                      <p class="price">
+                        kr 1 000
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 90%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-tekstiler-2.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Håndvarmer</h3>
+                      <p class="description">
+                        Håndstrikket håndvarmer av Marigarn
+                      </p>
+                      <p class="price">
+                        kr 500
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 25%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="grid-column grid-span-4">
+              <div class="box">
+                <a href="/butikk/1">
+                  <div class="image">
+                    <img src="assets/images/produkt-tekstiler-3.jpg">
+                  </div>
+                  <div class="text">
+                    <div class="text-wrapper">
+                      <h3 class="title">Kidmohairskjerf</h3>
+                      <p class="description">
+                        Håndstrikket skjerf av vårt eget Marigarn
+                      </p>
+                      <p class="price">
+                        kr 700
+                      </p>
+                      <div class="availability">
+                        <div class="bar" style="width: 90%"></div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
