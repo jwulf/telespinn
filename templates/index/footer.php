08868420eeee803ed2275e4854a5f86535faaca5
Lasse Brenden <lasse.brenden@icloud.com>
2013-11-24 13:14:19 +0100

    Push footer

--- templates/index/footer.php
+++ templates/index/footer.php
@@ -1,8 +1,49 @@
     <footer id="footer">
       <div class="container">
         <div class="grid-row">
-          <div class="grid-column grid-span-6">
-            Test
+          <div class="grid-column grid-span-3">
+            <ul class="list">
+              <li class="list-item">
+                <h1>Kundeservice</h1>
+              </li>
+              <li class="list-item">
+                <a href="/">Ofte stilte spørsmål</a>
+              </li>
+              <li class="list-item">
+                <a href="/">Leveringsinformasjon</a>
+              </li>
+              <li class="list-item">
+                <a href="/">Betalingsinformasjon</a>
+              </li>
+              <li class="list-item">
+                <a href="/">Rettigheter og betingelser</a>
+              </li>
+              <li class="list-item">
+                <a href="/">Personvern</a>
+              </li>
+            </ul>
+          </div>
+          <div class="grid-column grid-span-3">
+            <ul class="list">
+              <li class="list-item">
+                <h1>Kontakt oss</h1>
+              </li>
+              <li class="list-item">
+                Telespinn AS<br>
+                Blikavegen 22<br>
+                NO-3841 FLATDAL
+              </li>
+              <li class="list-item">
+                Besøk oss:<br>
+                Man–Fre kl. 08–16
+              </li>
+              <li class="list-item">
+                <a href="/">post@telespinn.no</a>
+              </li>
+              <li class="list-item">
+                <a href="/">35 70 94 00</a>
+              </li>
+            </ul>
           </div>
           <div class="grid-column grid-span-6">
             <img src="assets/images/map.png">
@@ -10,7 +51,7 @@
         </div>
         <div class="grid-row">
           <div class="grid-align-center grid-column grid-span-12">
-            &copy; 2013 <strong>Telespinn</strong>
+            &copy; 2013 <strong>Telespinn AS</strong>
           </div>
         </div>
       </div>
